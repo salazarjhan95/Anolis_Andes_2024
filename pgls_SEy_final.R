6dd9615bcc41e52efc668d6d316cbe2a4da24513
Jhan Carlos Salazar Salazar <61802315+salazarjhan95@users.noreply.github.com>
2024-09-28 22:33:15 -0500

    Update pgls_SEy_final.R

--- pgls_SEy_final.R
+++ pgls_SEy_final.R
@@ -6,7 +6,7 @@
 ######                                                              ######
 ##########################################################################
 
-setwd("C:/Users/jhanc/Box/Investigacion/Investigaciones/Side projects/Anolis/CT/Last_analysis/NewTree")
+setwd("")
 
 library(phytools)
 library(ape)
